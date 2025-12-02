ecdf512e15dc35d837d21ed5e742241e8afb4293
gbrandt117 <gustavbrandt117@gmail.com>
2025-12-02 09:35:35 +0100

    lavet en good day i greetings.js

--- frontend/greeting.js
+++ frontend/greeting.js
@@ -12,7 +12,9 @@ let text = "";
 // Bestem hilsen ud fra tidspunktet
 if (hour >= 6 && hour < 10) {
     text = "Goodmorning";
-} else if (hour >= 10 && hour < 18) {
+} else if (hour >= 10 && hour < 14) {
+    text = "Good day";
+} else if (hour >= 14 && hour < 18) {    
     text = "Good afternoon";
 } else if (hour >= 18 && hour < 23) {
     text = "Good evening";
