e6f61cf27591fc303d2088cc913510046ebcf0c0
luwam09habte <luwam1@hotmail.dk>
2025-12-03 12:54:42 +0100

    functionen knyttet til knappen

--- frontend/activity.js
+++ frontend/activity.js
@@ -8,10 +8,10 @@ if (userName) {
 } else {
     activityText.textContent = "What are you doing today?";
 }
-
+/*
 function go() {
     window.location.href = 'mood.html';
-}
+} */
 
 function ChooseActivity (id, name) {
     localStorage.setItem("selectedActivity", JSON.stringify({id, name})); /*localstorage gemmer lokalt så det kan bruges på næste side, 
--- frontend/mood.js
+++ frontend/mood.js
@@ -1,3 +1,21 @@
 function goplay() {
     window.location.href = 'play.html';
 }
+async function loadMoods() { // henter data fra backend (derfor async)
+    const activity = JSON.parse(localStorage.getItem ("selectedActivity"));
+
+    const response = await fetch('/api/moods/${activity.id}');
+    const moods = await response.json();
+
+    const container = document.getElementById("button-row");
+
+    for(let i = 0; i<moods.length; i++) {
+        const mood = moods[i];
+        container.innerHTML += `
+        <button data-id="${moods.id}">
+            ${moods.name}
+        </button>
+        `;
+    }
+}
+loadMoods();
